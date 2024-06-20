15074272a19b0d9c5fcf3b2397f68ce8fdd9a5ae
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2024-06-21 00:00:01 +0530

    chore: Add profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Profile from '../components/Profile'
+
+const profile = () => {
+  return (
+    <div>
+      <Profile />   
+    </div>
+  )
+}
+
+export default profile
